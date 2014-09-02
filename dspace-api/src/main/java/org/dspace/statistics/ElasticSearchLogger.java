b464e663e3dac9224a788ad3efd07b5ca4a273bf
Peter Dietz <peter@longsight.com>
2014-09-02 17:18:33 -0400

    Move Elastic Search stats mapping to resource json file

--- dspace-api/src/main/java/org/dspace/statistics/ElasticSearchLogger.java
+++ dspace-api/src/main/java/org/dspace/statistics/ElasticSearchLogger.java
@@ -8,6 +8,8 @@
 package org.dspace.statistics;
 
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import com.maxmind.geoip.Location;
 import com.maxmind.geoip.LookupService;
 import org.apache.commons.lang.exception.ExceptionUtils;
@@ -42,6 +44,7 @@ import org.elasticsearch.node.NodeBuilder;
 import javax.servlet.http.HttpServletRequest;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
 import java.util.*;
 
@@ -135,93 +138,12 @@ public class ElasticSearchLogger {
             //If elastic search index exists, then we are good to go, otherwise, we need to create that index. Should only need to happen once ever.
             log.info("DS ES index didn't exist, we need to create it.");
 
-            Settings settings = ImmutableSettings.settingsBuilder()
-                    .put("number_of_replicas", 1)
-                    .put("number_of_shards", 5)
-                    .put("cluster.name", clusterName)
-                    .build();
-            
-            String stringMappingJSON = "{\""+indexType+"\" : { \"properties\" : {\n" +
-                    "   \"userAgent\":{\n" +
-                    "      \"type\":\"string\"\n" +
-                    "   },\n" +
-                    "   \"countryCode\":{\n" +
-                    "      \"type\":\"string\",\n" +
-                    "      \"index\":\"not_analyzed\",\n" +
-                    "      \"omit_norms\":true\n" +
-                    "   },\n" +
-                    "   \"dns\":{\n" +
-                    "      \"type\":\"multi_field\",\n" +
-                    "      \"fields\": {\n" +
-                    "        \"dns\": {\"type\":\"string\",\"index\":\"analyzed\"},\n" +
-                    "        \"untouched\":{\"type\":\"string\",\"index\":\"not_analyzed\"}\n" +
-                    "      }\n" +
-                    "   },\n" +
-                    "   \"isBot\":{\n" +
-                    "      \"type\":\"boolean\"\n" +
-                    "   },\n" +
-                    "   \"owningColl\":{\n" +
-                    "      \"type\":\"integer\",\n" +
-                    "      \"index\":\"not_analyzed\"\n" +
-                    "   },\n" +
-                    "   \"type\":{\n" +
-                    "      \"type\":\"string\",\n" +
-                    "      \"index\":\"not_analyzed\",\n" +
-                    "      \"omit_norms\":true\n" +
-                    "   },\n" +
-                    "   \"owningComm\":{\n" +
-                    "      \"type\":\"integer\",\n" +
-                    "      \"index\":\"not_analyzed\"\n" +
-                    "   },\n" +
-                    "   \"city\":{\n" +
-                    "      \"type\":\"multi_field\",\n" +
-                    "      \"fields\": {\n" +
-                    "        \"city\": {\"type\":\"string\",\"index\":\"analyzed\"},\n" +
-                    "        \"untouched\":{\"type\":\"string\",\"index\":\"not_analyzed\"}\n" +
-                    "      }\n" +
-                    "   },\n" +
-                    "   \"country\":{\n" +
-                    "      \"type\":\"multi_field\",\n" +
-                    "      \"fields\": {\n" +
-                    "         \"country\": {\"type\":\"string\",\"index\":\"analyzed\"},\n" +
-                    "         \"untouched\":{\"type\":\"string\",\"index\":\"not_analyzed\"}\n" +
-                    "       }\n" +
-                    "   },\n" +
-                    "   \"ip\":{\n" +
-                    "      \"type\":\"multi_field\",\n" +
-                    "       \"fields\": {\n" +
-                    "         \"ip\": {\"type\":\"string\",\"index\":\"analyzed\"},\n" +
-                    "         \"untouched\":{\"type\":\"string\",\"index\":\"not_analyzed\"}\n" +
-                    "       }\n" +
-                    "   },\n" +
-                    "   \"id\":{\n" +
-                    "      \"type\":\"integer\",\n" +
-                    "      \"index\":\"not_analyzed\"\n" +
-                    "   },\n" +
-                    "   \"time\":{\n" +
-                    "      \"type\":\"date\"\n" +
-                    "   },\n" +
-                    "   \"owningItem\":{\n" +
-                    "      \"type\":\"string\",\n" +
-                    "      \"index\":\"not_analyzed\"\n" +
-                    "   },\n" +
-                    "   \"continent\":{\n" +
-                    "      \"type\":\"string\",\n" +
-                    "      \"index\":\"not_analyzed\"\n" +
-                    "   },\n" +
-                    "   \"geo\":{\n" +
-                    "      \"type\":\"geo_point\"\n" +
-                    "   },\n" +
-                    "   \"bundleName\":{\n" +
-                    "      \"type\":\"string\",\n" +
-                    "      \"index\":\"not_analyzed\"\n" +
-                    "   },\n" +
-                    "   \"epersonid\":{\n" +
-                    "      \"type\":\"string\",\n" +
-                    "      \"index\":\"not_analyzed\"\n" +
-                    "   }\n" +
-                    "} } }";
+            String mappingPath = ElasticSearchLogger.class.getPackage().getName().replaceAll("\\.", "/") ;
+            URL url = Resources.getResource(mappingPath + "/elasticsearch-statistics-mapping.json");
+            String stringMappingJSON = Resources.toString(url, Charsets.UTF_8);
+            stringMappingJSON = stringMappingJSON.replace("stats", indexType);
 
+            //Necessary to post, in order for index/type to be created.
             client.prepareIndex(indexName, indexType, "1")
                     .setSource(XContentFactory.jsonBuilder()
                                 .startObject()
