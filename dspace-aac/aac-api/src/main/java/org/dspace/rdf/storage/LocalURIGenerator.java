2493ef03296b4357180f97450c58247043254044
Mark Diggory <mdiggory@atmire.com>
2016-02-15 16:27:34 -0500

    w2p-31088 1.3. Improve Representation of DSpace Entities in LOD RDF - Override original convertors to make customizations.

---
+++ dspace-aac/aac-api/src/main/java/org/dspace/rdf/storage/LocalURIGenerator.java
@@ -0,0 +1,67 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+
+package org.dspace.rdf.storage;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.dspace.content.DSpaceObject;
+import org.dspace.content.Site;
+import org.dspace.core.Constants;
+import org.dspace.core.Context;
+import org.dspace.rdf.RDFConfiguration;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Pascal-Nicolas Becker (dspace -at- pascal -hyphen- becker -dot- de)
+ */
+public class LocalURIGenerator implements URIGenerator {
+    private static final Logger log = Logger.getLogger(LocalURIGenerator.class);
+
+    @Override
+    public String generateIdentifier(Context context, int type, int id, 
+            String handle, String[] identifiers)
+            throws SQLException
+    {
+        String urlPrefix = RDFConfiguration.getDSpaceRDFModuleURI() + "/resource/";
+        
+        if (type == Constants.SITE)
+        {
+            return urlPrefix + Site.getSiteHandle();
+        }
+        
+        if (type == Constants.COMMUNITY 
+                || type == Constants.COLLECTION 
+                || type == Constants.ITEM)
+        {
+            if (StringUtils.isEmpty(handle))
+            {
+                throw new IllegalArgumentException("Handle is null");
+            }
+            return urlPrefix + handle;
+        }
+        
+        return null;
+    }
+
+    @Override
+    public String generateIdentifier(Context context, DSpaceObject dso) throws SQLException {
+        if (dso.getType() != Constants.SITE
+                && dso.getType() != Constants.COMMUNITY
+                && dso.getType() != Constants.COLLECTION
+                && dso.getType() != Constants.ITEM)
+        {
+            return null;
+        }
+        
+        return generateIdentifier(context, dso.getType(), dso.getID(), dso.getHandle(), dso.getIdentifiers(context));
+    }
+
+}
